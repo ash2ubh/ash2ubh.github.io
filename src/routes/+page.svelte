0ce6fc17d5e54ddc41343a2cc3527ac106d36dca
ash2ubh <winoramaputra@protonmail.com>
2025-12-04 23:26:56 +0700

    modified:   src/routes/+page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     <div style="margin: 0; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); width: 50%; height: 50%; border-radius: 2rem; box-shadow: 0 0 3rem #ff4545; display: flex; justify-content: center; justify-items: center; text-align: center;">
         <div style="display: inline-block; position: relative; width: 49.5%; height: 100%; margin: 0; padding: 0;">
             <div style="display: block; position: relative; top: 50%; transform: translateY(-50%);">
-                <p style="display: block; position: relative; margin-top: 0; font-size: 3rem; color: #a6a6a6;">
+                <p style="display: block; position: relative; margin-top: 0; font-size: 2rem; color: #a6a6a6;">
                     ash2ubh
                 </p>
                 <p style="display: block; position: relative; font-size: 0.8rem; margin-bottom: 0; color: #a6a6a6;">
